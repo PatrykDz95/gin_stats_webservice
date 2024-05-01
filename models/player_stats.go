77211ccbc4e07040a506ef586333ade895a2f614
Patryk <rastawafel03@gmail.com>
2024-05-01 16:48:55 +0200

    feat: added request validation and finding player by name and surname

--- models/player_stats.go
+++ models/player_stats.go
@@ -1,10 +1,14 @@
 package models
 
+import (
+	"github.com/go-playground/validator/v10"
+)
+
 type PlayerStats struct {
 	ID        int    `gorm:"primary_key" json:"id"`
-	Name      string `json:"name"`
-	Surname   string `json:"surname"`
-	Age       int    `json:"age"`
+	Name      string `json:"name" validate:"required,min=3,max=20"`
+	Surname   string `json:"surname" validate:"required,min=3,max=20"`
+	Age       int    `json:"age" validate:"required,min=18,max=50"`
 	Position  string `json:"position"`
 	Team      string `json:"team"`
 	Height    int    `json:"height"`
@@ -17,4 +21,12 @@ type PlayerStats struct {
 	Turnovers int    `json:"turnovers"`
 	Minutes   int    `json:"minutes"`
 	CreatedOn string `json:"created_on"`
+}
+
+func (p *PlayerStats) Validate() error {
+	validate := validator.New()
+	if err := validate.Struct(p); err != nil {
+		return err
+	}
+	return nil
 }
--- routers/player_stats.go
+++ routers/player_stats.go
@@ -2,7 +2,6 @@ package routes
 
 import (
 	"gin/database"
-	_ "gin/database"
 	"gin/models"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -14,9 +13,6 @@ func SetupRouter() *gin.Engine {
 
 	// Initialize database connection
 	database.InitDB()
-
-	// Route to handle inserting data
-
 	return router
 }
 
@@ -25,6 +21,11 @@ func Add(c *gin.Context) {
 	playerStats.CreatedOn = time.Now().Format("2006-01-02 15:04:05")
 	if err := c.ShouldBindJSON(&playerStats); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"errorMessage": err.Error()})
+		return
+	}
+	// Validate the player stats data
+	if err := playerStats.Validate(); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 	created := database.DB.Create(&playerStats)
@@ -45,7 +46,7 @@ func GetAll(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"players": found.Statement.Model})
 }
 
-func Get(c *gin.Context) {
+func GetById(c *gin.Context) {
 	playerStats := models.PlayerStats{}
 	found := database.DB.First(&playerStats, c.Param("id"))
 	if found.Error != nil {
@@ -56,10 +57,24 @@ func Get(c *gin.Context) {
 
 }
 
+func GetByNameAndSurname(c *gin.Context) {
+	var playerStats []models.PlayerStats
+	found := database.DB.Where("name = ? AND surname = ?", c.Param("name"), c.Param("surname")).Find(&playerStats)
+	if found.Error != nil {
+		c.JSON(http.StatusNotFound, gin.H{"errorMessage": found.Error.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"player": found.Statement.Model})
+}
+
 func Update(c *gin.Context) {
 	playerStats := models.PlayerStats{}
 	if err := c.ShouldBindJSON(&playerStats); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"errorMessage": err.Error()})
+		return
+	}
+	if err := playerStats.Validate(); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 	updated := database.DB.Model(&models.PlayerStats{}).Where("id = ?", c.Param("id")).Updates(playerStats)
